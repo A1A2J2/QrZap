d4884ee48fbcded3aa7afd0a4c991c236e41d7c0
Arham Siva <arhamsiva@gmail.com>
2026-06-02 13:26:15 -0700

    Opening Special

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -6,6 +6,11 @@ import '../styles/globals.css';
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+      <div className="bg-indigo-600 px-4 py-3 text-white">
+        <p className="text-center text-sm font-medium">
+          Opening special: All users get premium for free if they sign up before July 31st, and they get it permanently!
+        </p>
+      </div>
       <Navbar />
       <Component {...pageProps} />
     </ThemeProvider>
